6c44d7c055fc325b51992fbda9fd5c4c6af182a4
Diem Vu <vuthingocdiem95@gmail.com>
2021-03-24 20:00:25 -0700

    second commit of ListManipulator.java

--- Assignment5/ListManipulator.java
+++ Assignment5/ListManipulator.java
@@ -4,6 +4,7 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Class ListManipulator
@@ -11,13 +12,13 @@ import java.util.Map;
  * java.util.ArrayList<Integer>
  * java.util.HashMap<Integer>
  * java.util.Map<Integer>
- * 
- * 
+ *
+ *
  * Methods:
- * ArrayList<Integer>   sort                
+ * ArrayList<Integer>   sort
  * ArrayList<Integer>   swapLargestSmallest
  * void                 table
- * 
+ *
  * @author Diem.Vu
  * @version 1.0
  * @since 03.22.21
@@ -31,17 +32,17 @@ public class ListManipulator {
      * @return
      */
     public ArrayList<Integer> sort(ArrayList<Integer> myLst, Boolean ascending){
-        
+
         ArrayList <Integer> sorted = new ArrayList<>(myLst.size());
-        
+
         ArrayList <Integer> tempList = new ArrayList<>(myLst.size());
         for(int i = 0; i < myLst.size(); i++){
-                tempList.add(myLst.get(i));
+            tempList.add(myLst.get(i));
         }
-       
+
         for(int i = 0; i < tempList.size() - 2; i++){
             for(int j = tempList.size() - 1; j > i; j--){
-                
+
                 if(tempList.get(i).compareTo(tempList.get(j)) > 0){
                     int temp = tempList.get(i);
                     tempList.set(i, tempList.get(j));
@@ -49,7 +50,7 @@ public class ListManipulator {
                 }
             }
         }
-        
+
         if(ascending){
             for(int i = 0; i < tempList.size(); i++){
                 sorted.add(tempList.get(i));
@@ -70,8 +71,8 @@ public class ListManipulator {
     public ArrayList<Integer> swapLargestSmallest(ArrayList<Integer> myLst) {
         ArrayList <Integer> swap = new ArrayList<>(myLst.size());
         for(int i = 0; i < myLst.size(); i++){
-                swap.add(myLst.get(i));
-            }
+            swap.add(myLst.get(i));
+        }
         int max = swap.get(0), min = swap.get(0);
         int maxIndex = 0, minIndex = 0;
         for (int n : swap) {
@@ -86,26 +87,27 @@ public class ListManipulator {
         }
         swap.set(minIndex, max);
         swap.set(maxIndex, min);
-        
+
         return swap;
     }
-    
+
     /**
      *
      * @param myLst
      */
     public void table(ArrayList<Integer> myLst){
-        
-        HashMap<Integer, Integer> table = new HashMap<>();
-        Map<Integer, Integer> hm = new HashMap<>();
-        for (Integer i : myLst){
-            Integer j = hm.get(i);
-            hm.put(i, (j==null)? 1: j+1);
-        }
-        
-        for (Map.Entry<Integer,Integer> val: hm.entrySet()){
-            System.out.println("Element " + val.getKey() + " " + "occurs" + ": " + val.getValue() + " times");
-        }
-      
+        //create a table from ArrayList myLst
+        Map<Integer, Integer> table = new HashMap<>();
+        myLst.forEach((i) -> {
+            Integer j = table.get(i);
+            table.put(i, (j == null)? 1: j+1); //put element i of myLst and its occurrence (1 or j+1) to the table
+        });
+        //sort the table
+        TreeMap<Integer, Integer> sortedTb = new TreeMap<>(table);
+        //output the table to command lines
+        sortedTb.forEach((k, v) -> {
+            System.out.println("Element " + k + " " + "occurs" + ": " + v + " times");
+        });
+
     }
 }
